2f9c10c46189fe0424356ec9377f1ce38ef5edf9
Diego Jonguitud DellPc <djonguitud95@gmail.com>
2022-09-23 01:59:18 -0500

    adding all about responsive style

--- code/script.js
+++ code/script.js
@@ -2,15 +2,31 @@
 
 let behOne = window.pageYOffset;
 
-window.onscroll = function() {
+window.onscroll = function () {
+	let behTwo = window.pageYOffset;
 
-    let behTwo = window.pageYOffset;
+	if (behOne > behTwo) {
+		document.getElementById("navHide").style.top = "0";
+	} else {
+		document.getElementById("navHide").style.top = "-100%";
+	}
 
-    if (behOne > behTwo) {
-      document.getElementById("navHide").style.top = "0";
-    } else {
-      document.getElementById("navHide").style.top = "-100%";
-    }
+	behOne = behTwo;
+};
 
-    behOne = behTwo;
-  } 
+/* Change slogan to different languajes */
+let sloganWelcome = [
+	"HELLO! I AM DIEGO. FULLSTACK WEB DEVELOPER",
+	"HOLA! SOY DIEGO. DESARROLLADOR WEB FULLSTACK",
+	"SALUT! JE SUIS DIEGO. DÉVELOPPEUR WEB FULLSTACK",
+	"CIAO! IO SONO DIEGO. SVILUPPATORE WEB COMPLETO",
+	"HALLO! ICH BIN DIEGO. FULLSTACK-WEBENTWICKLER",
+];
+
+let sloganRandom = function (max) {
+	return Math.floor(Math.random() * max);
+};
+
+let getIndex = sloganWelcome[sloganRandom(sloganWelcome.length)];
+
+let el = document.getElementById("langChange");
