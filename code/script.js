9f51df602e36e046f6ace17263578bee5292dcff
Diego Jonguitud DellPc <djonguitud95@gmail.com>
2022-09-24 13:32:24 -0500

    adding comments to html5 file

--- code/script.js
+++ code/script.js
@@ -13,20 +13,3 @@ window.onscroll = function () {
 
 	behOne = behTwo;
 };
-
-/* Change slogan to different languajes */
-let sloganWelcome = [
-	"HELLO! I AM DIEGO. FULLSTACK WEB DEVELOPER",
-	"HOLA! SOY DIEGO. DESARROLLADOR WEB FULLSTACK",
-	"SALUT! JE SUIS DIEGO. DÉVELOPPEUR WEB FULLSTACK",
-	"CIAO! IO SONO DIEGO. SVILUPPATORE WEB COMPLETO",
-	"HALLO! ICH BIN DIEGO. FULLSTACK-WEBENTWICKLER",
-];
-
-let sloganRandom = function (max) {
-	return Math.floor(Math.random() * max);
-};
-
-let getIndex = sloganWelcome[sloganRandom(sloganWelcome.length)];
-
-let el = document.getElementById("langChange");
